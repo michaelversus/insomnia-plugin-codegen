da4bf30d36b6e202678c4e2fdb567b6efcbbeb62
Michalis Karagiorgos <michaliskarag@gmail.com>
2020-03-13 20:09:42 +0200

    export har for every request starting with CodeGen name

---
+++ src/plugin.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const homedir = require("os").homedir();
+
+
+
+module.exports.workspaceActions = [
+    {
+        label: "My Plugin Action",
+        icon: "fa-star",
+        action: async (context, models) => {
+            //const filtered = models.requestGroups.filter(d => d.name == "CodeGen");
+            const ex = await context.data.export.har({
+                includePrivate: false,
+                workspace: models.workspace
+            });
+            const parsed = JSON.parse(ex);
+            const filteredEntries = parsed.log.entries.filter(
+                d => d.comment.lastIndexOf("CodeGen", 0) === 0
+            );
+            parsed.log.entries = filteredEntries;
+            //results.push(`<li>${models.workspace}</li>`);
+            //const html = `<ul>${results.join("\n")}</ul>`;
+            //context.app.showGenericModalDialog("Results", { html });
+            const username = homedir.split("/")[2];
+            fs.writeFileSync(
+                `/users/${username}/Desktop/export.json`,
+                JSON.stringify(parsed)
+            )
+        }
+    }
+];
