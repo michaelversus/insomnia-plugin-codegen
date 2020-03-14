087b96b7bec217a466e01f64d448c0ec2fa425cb
Michalis Karagiorgos <michaliskarag@gmail.com>
2020-03-14 17:53:04 +0200

    add second action for kotlin

--- src/plugin.js
+++ src/plugin.js
@@ -5,8 +5,8 @@ var HTTPSnippet = require('httpsnippet');
 
 module.exports.workspaceActions = [
     {
-        label: "export swift code",
-        icon: "fa-file-code-o",
+        label: "generate swift code",
+        icon: "fa-apple",
         action: async (context, models) => {
             const ex = await context.data.export.har({
                 includePrivate: false,
@@ -31,6 +31,38 @@ module.exports.workspaceActions = [
                 const filename = filteredEntries[i].comment.replace("CodeGen-", "")
                 fs.writeFileSync(
                     `/users/${username}/Desktop/${filename}.swift`,
+                    code
+                )
+            }
+        }
+    },
+    {
+        label: "generate kotlin code",
+        icon: "fa-android",
+        action: async (context, models) => {
+            const ex = await context.data.export.har({
+                includePrivate: false,
+                workspace: models.workspace
+            });
+            const parsed = JSON.parse(ex);
+
+            //filter the entries with comment starting with CodeGen prefix
+            const filteredEntries = parsed.log.entries.filter(
+                d => d.comment.lastIndexOf("CodeGen-", 0) === 0
+            );
+            const username = homedir.split("/")[2];
+
+            for (var i = 0; i < filteredEntries.length; i += 1) {
+                parsed.log.entries = [filteredEntries[i]];
+
+                //create the swift snippet
+                var snippet = new HTTPSnippet(parsed)
+                const code = snippet.convert('kotlin', 'custom')
+
+                //save file to users desktop
+                const filename = filteredEntries[i].comment.replace("CodeGen-", "")
+                fs.writeFileSync(
+                    `/users/${username}/Desktop/${filename}.kt`,
                     code
                 )
             }
