e31f99ebda8d4fe6c0a66547d13976ca87c576e1
Michalis Karagiorgos <michaliskarag@gmail.com>
2020-03-14 15:40:19 +0200

    update icon and export multiple swift files

--- src/plugin.js
+++ src/plugin.js
@@ -5,31 +5,35 @@ var HTTPSnippet = require('httpsnippet');
 
 module.exports.workspaceActions = [
     {
-        label: "My Plugin Action",
-        icon: "fa-star",
+        label: "export swift code",
+        icon: "fa-file-code-o",
         action: async (context, models) => {
-            //const filtered = models.requestGroups.filter(d => d.name == "CodeGen");
             const ex = await context.data.export.har({
                 includePrivate: false,
                 workspace: models.workspace
             });
             const parsed = JSON.parse(ex);
-            const filteredEntries = parsed.log.entries.filter(
-                d => d.comment.lastIndexOf("CodeGen", 0) === 0
-            );
-            parsed.log.entries = filteredEntries;
-            //results.push(`<li>${models.workspace}</li>`);
-            //const html = `<ul>${results.join("\n")}</ul>`;
-            //context.app.showGenericModalDialog("Results", { html });
-            const username = homedir.split("/")[2];
-            const har = JSON.stringify(parsed)
-            var snippet = new HTTPSnippet(har)
-            const code = snippet.convert('swift', 'nsurlsession')
 
-            fs.writeFileSync(
-                `/users/${username}/Desktop/Export.swift`,
-                code
-            )
+            //filter the entries with comment starting with CodeGen prefix
+            const filteredEntries = parsed.log.entries.filter(
+                d => d.comment.lastIndexOf("CodeGen-", 0) === 0
+            );
+            const username = homedir.split("/")[2];
+
+            for (var i = 0; i < filteredEntries.length; i += 1) {
+                parsed.log.entries = [filteredEntries[i]];
+
+                //create the swift snippet
+                var snippet = new HTTPSnippet(parsed)
+                const code = snippet.convert('swift', 'nsurlsession')
+
+                //save file to users desktop
+                const filename = filteredEntries[i].comment.replace("CodeGen-", "")
+                fs.writeFileSync(
+                    `/users/${username}/Desktop/${filename}.swift`,
+                    code
+                )
+            }
         }
     }
 ];
